d7de7170a5ab3fa1d85806cda3abf9281c66924a
cpprogues <cpprogues@no-reply>
2021-10-11 23:11:44 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+import asyncio
+import csv
+import os
+from pathlib import Path
+from random import choice
+
+from dotenv import load_dotenv
+import nextcord
+from nextcord.ext import commands
+from pytube import YouTube
+
+load_dotenv()
+
+song_list = list(csv.DictReader(open('eurobeat.csv')))
+bot = commands.Bot('!')
+
+@bot.event
+async def on_ready():
+    print(f'User: {bot.user.name}#{bot.user.discriminator}')
+    print('Guilds:')
+    print('\n'.join([f'  - {guild.name}' for guild in bot.guilds]))
+
+
+@bot.command()
+async def eurobeat(ctx):
+    if ctx.author.bot:
+        return
+
+    voice_state = ctx.author.voice
+    if voice_state == None or voice_state.channel == None:
+        await ctx.reply('You\'re not currently connected to a voice channel')
+        return
+
+    voice_channel = voice_state.channel
+    voice_client = await voice_channel.connect()
+    print(f'Now connected to #{voice_channel.name} on {voice_channel.guild.name}')
+
+    asyncio.get_event_loop().create_task(__play_next(ctx))
+
+@bot.command()
+async def drift(ctx):
+    if ctx.author.bot:
+        return
+
+    voice_client = ctx.voice_client
+    if voice_client == None:
+        await ctx.reply('Not connected to any voice channels :(')
+        return
+
+    voice_client.stop()
+
+@bot.command()
+async def stopthemusic(ctx):
+    if ctx.author.bot:
+        return
+
+    if ctx.voice_client == None:
+        return
+
+    await ctx.voice_client.disconnect()
+    await ctx.reply('https://www.youtube.com/watch?v=sFODclG8mBY')
+
+async def __play_next(ctx):
+    if ctx.voice_client == None or not ctx.voice_client.is_connected():
+        return
+
+    voice_client = ctx.voice_client
+    voice_channel = voice_client.channel
+
+    random_eurobeat = choice(song_list)
+    title = random_eurobeat['title']
+    artist = random_eurobeat['artist']
+    url = random_eurobeat['url']
+
+    stream = YouTube(url).streams \
+        .filter(only_audio=True).order_by('abr').desc().first()
+    download_path = Path('downloads').joinpath(stream.default_filename)
+
+    # Jank caching mechanism
+    if not download_path.is_file():
+        stream.download('downloads')
+        print(f'Finished download: {download_path}')
+    else:
+        print(f'Cached file found: {download_path}')
+
+    _loop = asyncio.get_event_loop()
+
+    def on_after(error):
+        _loop.create_task(__play_next(ctx))
+
+    audio_source = await nextcord.FFmpegOpusAudio.from_probe(download_path)
+    voice_client.play(audio_source, after=on_after)
+
+    title = random_eurobeat['title']
+    artist = random_eurobeat['artist']
+    print(f'Playing {title} in #{voice_channel.name} on {voice_channel.guild.name}')
+    await ctx.send(f'Now playing: {title} by {artist}\n{url}')
+
+if __name__ == '__main__':
+    bot.run(os.environ['TOKEN'])
